33c6a31334f9591060d077d9c13c193a47df8fd9
sk1es <144143057+sk1ees@users.noreply.github.com>
2024-09-07 22:31:27 +0530

    logo-animated

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const formLeft = document.querySelector(".formLeft");
 const navLeftV2Items = document.querySelector(".navLeftV2");
 const title = document.querySelector(".title");
 const footer = document.querySelector(".footer");
+const icon = document.querySelector(".icon");
 
 async function scaleImage() {
   await fadeElement();
@@ -10,6 +11,7 @@ async function scaleImage() {
   title.classList.toggle("fadeLeft");
   footer.classList.toggle("fadeDown");
   imgDiv.classList.toggle("scale");
+  icon.classList.toggle("titleMove");
 }
 function fadeElement() {
   return new Promise((resolve) => {
